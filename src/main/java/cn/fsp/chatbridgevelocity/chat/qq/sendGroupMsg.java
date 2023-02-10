3f1622402484a7e65c8e145aae234ff2020773a6
tangsu99 <18763952906@163.com>
2023-02-10 17:48:16 +0800

    优化类位置

--- src/main/java/cn/fsp/chatbridgevelocity/chat/qq/sendGroupMsg.java
+++ src/main/java/cn/fsp/chatbridgevelocity/chat/qq/sendGroupMsg.java
@@ -1,14 +1,6 @@
-package cn.fsp.chatbridgevelocity.chat.qq.API;
+package cn.fsp.chatbridgevelocity.chat.qq;
 
 public class sendGroupMsg {
-    //        {
-//        "action": "终结点名称, 例如 'send_group_msg'",
-//        "params": {
-//                "参数名": "参数值",
-//                "参数名2": "参数值"
-//        },
-//        "echo": "'回声', 如果指定了 echo 字段, 那么响应包也会同时包含一个 echo 字段, 它们会有相同的值"
-//    }
     String action = "send_group_msg";
     Params params;
     String echo;
